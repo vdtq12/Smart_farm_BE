3489311ba7814ec7aece17bcb4b426761ba87c5e
Quyen Vu <111192176+vdtq12@users.noreply.github.com>
2023-05-10 17:20:16 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,208 @@
+const express = require('express')
+const morgan = require('morgan')
+const axios = require('axios')
+const mqtt = require('mqtt')
+const mongoose = require('mongoose')
+const moment = require('moment-timezone')
+const { HumidModel, MoistModel, TempModel, PumpModel } = require('./config/data')
+const cors = require('cors')
+const WebSocket = require('ws')
+const http = require('http')
+
+
+const app = express()
+app.use(cors())
+app.use(morgan('combined'))
+
+
+
+const port = 3000
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+
+wss.on('connection', function connection(ws) {
+    console.log('--------- Client connected')
+    
+
+  const interval = setInterval( () => {
+    axios
+          .get(`https://io.adafruit.com/api/v2/dadn222/feeds`, {
+            params: {
+              'x-aio-key': process.env.ADAKEY,
+            },
+          })
+          .then((res) => {
+            // console.log(res.data)
+            res.data.forEach( async element => {
+                if (element.name === "humid"){
+
+                    const instance = new HumidModel()
+                    // console.log(element)
+                    instance.value = element.last_value
+                    instance.time = moment(element.updated_at).tz("Asia/Ho_Chi_Minh").format("HH:mm:ss")
+                    instance.name = element.name
+                    await HumidModel.findOne({time: instance.time}).lean().then( async data => {
+                      // console.log(data)
+                      if (data === null) {
+                        await instance.save()
+                        await HumidModel.find().sort({_id: -1}).limit(5).lean().then(data => {
+                          ws.send(JSON.stringify(data))
+                          // console.log(data)
+                        })
+                      }
+                    })
+    
+                }
+                else if (element.name === "moist"){
+
+                  const instance = new MoistModel()
+                  // console.log(element)
+                  instance.value = element.last_value
+                  instance.time = moment(element.updated_at).tz("Asia/Ho_Chi_Minh").format("HH:mm:ss")
+                  instance.name = element.name
+                  MoistModel.findOne({time: instance.time}).lean().then(async data => {
+                    // console.log(data)
+                    if (data === null) {
+                      await instance.save()
+                      await MoistModel.find().sort({_id: -1}).limit(5).lean().then(data => {
+                        ws.send(JSON.stringify(data))
+                        // console.log(data)
+                      })
+                    }
+                  })
+  
+                }
+                else if (element.name === "temp"){
+
+                  const instance = new TempModel()
+                  // console.log(element)
+                  instance.value = element.last_value
+                  instance.time = moment(element.updated_at).tz("Asia/Ho_Chi_Minh").format("HH:mm:ss")
+                  instance.name = element.name
+                  TempModel.findOne({time: instance.time}).lean().then(async data => {
+                    // console.log(data)
+                    if (data === null) {
+                      await instance.save()
+                      await TempModel.find().sort({_id: -1}).limit(5).lean().then(data => {
+                        ws.send(JSON.stringify(data))
+                        // console.log(data)
+                      })
+                    }
+                  })
+                
+                }
+                else if (element.name === "pump"){
+                  const instance = new PumpModel()
+                  // console.log(element)
+                  instance.time = moment(element.updated_at).tz("Asia/Ho_Chi_Minh").format("HH:mm:ss")
+                  instance.name = element.name
+                  if (element.last_value === '0') {
+                    instance.status = "Off"
+                  }
+                  else if (element.last_value === '1') {
+                    instance.status = "On"
+                  }
+                  PumpModel.findOne({time: instance.time}).lean().then(async data => {
+                    // console.log(data)
+                    if (data === null) {
+                      await instance.save()
+                      await PumpModel.find().sort({_id: -1}).limit(5).lean().then(data => {
+                        ws.send(JSON.stringify(data))
+                        console.log('data sent')
+                      })
+                    }
+                  })
+                }
+            })
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+    }, 1000);
+    
+  });
+
+
+server.listen(port, function(err) {
+    if (err) {
+        throw err;
+    }
+    console.log(`listening on port ${port}!`);
+    // TempModel.deleteMany().then(function(){
+    //       console.log("Data deleted"); // Success
+    //   }).catch(function(error){
+    //       console.log(error); // Failure
+    //   });
+    // console.log(server)
+});
+
+
+app.get('/humidData', (req, res) => {
+  HumidModel.find().sort({_id: -1}).limit(5).lean().then(data => res.send(data))
+})
+
+app.get('/moistData', (req, res) => {
+    MoistModel.find().sort({_id: -1}).limit(5).lean().then(data => res.send(data))
+})
+
+app.get('/tempData', (req, res) => {
+    TempModel.find().sort({_id: -1}).limit(5).lean().then(data => res.send(data))
+})
+
+app.get('/pumpData', (req, res) => {
+  PumpModel.find().sort({_id: -1}).limit(5).lean().then(data => res.send(data))
+})
+
+
+app.post('/clicked', (req, res) => {
+  axios
+          .get(`https://io.adafruit.com/api/v2/dadn222/feeds`, {
+            params: {
+              'x-aio-key': process.env.ADAKEY,
+            },
+          })
+          .then((res) => {
+            // console.log(res)
+            var changeState = -1
+
+            res.data.forEach(element => {
+              if (element.name === "pump"){
+                if (element.last_value === "0"){
+                  changeState = 1
+                }
+                else if (element.last_value === "1"){
+                  changeState = 0
+                }
+              }
+            })
+            
+            axios
+              .post(
+                `https://io.adafruit.com/api/v2/dadn222/feeds/pump/data`,
+                {
+                  value: changeState
+                },
+                {
+                  params: {
+                    'x-aio-key': process.env.ADAKEY,
+                  },
+                }
+              )
+              .then(() => {
+                console.log('success')
+              });
+            })
+
+          .catch((err) => {
+            console.log(err);
+          });
+
+})
+
